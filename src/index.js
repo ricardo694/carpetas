c03f43fb4d775036d19939b1af20ac1ad1a5e3ce
ricardo694 <ricardostiven9@gmail.com>
2025-05-21 07:09:16 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
+const fs=require('fs')
 
-const fs = require('fs');
+var nombreCarpeta= "Carpeta Nueva"
+var nombreArchivo= "./Carpeta Nueva/Archivo Nuevo.txt"
+var mensaje="Hola"
 
-const carpeta = './carpeta';     
-const archivo = carpeta + '/mensaje.txt'; 
-const mensaje = 'Hola';
+if(fs.existsSync(nombreCarpeta)){
 
-if (fs.existsSync(archivo)) {
-  
-  fs.unlinkSync(archivo);
-  console.log(' Archivo eliminado');
+    if(fs.existsSync(nombreArchivo)){
+        fs.unlink(nombreArchivo, (error)=>{
+             if(error){
+               throw error
+            }
+            console.log("Se elimino el archivo")
 
+            if(fs.existsSync(nombreCarpeta)){
+                fs.rmdir(nombreCarpeta, (error)=>{
+                    if(error){
+                        throw error
+                    }
+                    console.log("Se elimino la carpeta")
+                })
+            }
+        }) 
+    }
+    else{
+        fs.writeFile(nombreArchivo, '', (error)=>{
+            if(error){
+                throw error
+            }
+            console.log("Archivo Creado")
 
-  fs.rmdirSync(carpeta);
-  console.log(' Carpeta eliminada');
-} else {
+            fs.writeFile(nombreArchivo, mensaje, (error)=>{
+                if(error){
+                    throw error
+                }
+                console.log("Se escribio en el archivo creado")
+            })
 
-  if (!fs.existsSync(carpeta)) {
-    fs.mkdirSync(carpeta);
-    console.log(' Carpeta creada');
-  }
-
-  fs.writeFileSync(archivo, mensaje);
-  console.log(' Archivo creado con mensaje');
+        })
+    }
+}else{
+    fs.mkdir(nombreCarpeta, (error)=>{
+        if(error){
+            console.log(error.message)
+        }
+        console.log("Se creo una carpeta")
+        
+    })
 }
